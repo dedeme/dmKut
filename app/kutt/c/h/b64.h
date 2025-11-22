f557b21b065ed4d7f2b62b051f9000a05d96488d
Deme <deme@Main.deme.es>
2025-11-22 14:24:48 +0100

    Add libmkt-2024.02

--- app/kutt/c/h/b64.h
+++
@@ -1,9 +0,0 @@
-
-// b64 ---------------------------------
-
-// s -> s
-Val __b64_decode (char *pos, Val b64);
-
-// s -> s
-Val __b64_encode (Val s);
-
--- app/kutt/kutt/tests/writer/_main.sh
+++
@@ -1,5 +0,0 @@
-
-#kutt src/t0
-#kutt src/t1
-kutt src/t2
-
